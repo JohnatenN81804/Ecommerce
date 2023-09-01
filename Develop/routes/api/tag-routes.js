cacf5a4f666fe087921107535fef6774b5aedde5
JohnatenN81804 <137320162+JohnatenN81804@users.noreply.github.com>
2023-09-01 10:39:06 -0500

    pushing all files to git

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+// GET all tags
+router.get('/', async (req, res) => {
+  try {
+    // Find all tags and include associated Product data
+    const tags = await Tag.findAll({
+      include: { model: Product, through: ProductTag }
+    });
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET one tag by ID
+router.get('/:id', async (req, res) => {
+  try {
+    // Find a single tag by its `id` and include associated Product data
+    const tag = await Tag.findByPk(req.params.id, {
+      include: { model: Product, through: ProductTag }
+    });
+    if (!tag) {
+      res.status(404).json({ message: 'Tag not found' });
+      return;
+    }
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST a new tag
+router.post('/', async (req, res) => {
+  try {
+    // Create a new tag
+    const tag = await Tag.create(req.body);
+    res.status(201).json(tag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// PUT (update) a tag's name by ID
+router.put('/:id', async (req, res) => {
+  try {
+    // Update a tag's name by its `id` value
+    const updatedTag = await Tag.update(req.body, {
+      where: { id: req.params.id }
+    });
+    if (!updatedTag[0]) {
+      res.status(404).json({ message: 'Tag not found' });
+      return;
+    }
+    res.status(200).json({ message: 'Tag updated successfully' });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// DELETE a tag by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    // Delete one tag by its `id` value
+    const deletedTag = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+    if (!deletedTag) {
+      res.status(404).json({ message: 'Tag not found' });
+      return;
+    }
+    res.status(200).json({ message: 'Tag deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
